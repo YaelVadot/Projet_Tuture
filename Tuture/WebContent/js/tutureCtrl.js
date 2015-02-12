39c6d1a5354b235348cbbabce73c0befd9c7b78e
Ligo <Ligo@Ligo-PC>
2015-02-12 14:46:38 +0100

    Mise en place correcte du controlleur Essai mapping des objets

--- Tuture/WebContent/js/tutureCtrl.js
+++ Tuture/WebContent/js/tutureCtrl.js
@@ -5,56 +5,18 @@
 /**
  * Déclaration de l'application demoApp
  */
-var tutureApp = angular.module('tutureApp', [
-    // Dépendances du "module"
-    'homeCtrl'
-]);
+var tutureApp = angular.module('tutureApp', []);
 
 
-tutureApp.controller('homeCtrl', ['$scope',
-     function ($scope) {
+tutureApp.controller('homeCtrl', function ($scope,$http, $log) {
+	$scope.vitesseA = '0';
+	$scope.addVoiture = function(id) {
 
-        /* // Pour manipuler plus simplement les todos au sein du contrôleur
-         // On initialise les todos avec un tableau vide : []
-         var todos = $scope.todos = [];
-     
-		//Ajouter un todo
-		$scope.addTodo = function () {
-		    // .trim() permet de supprimer les espaces inutiles
-		    // en début et fin d'une chaîne de caractères
-		    var newTodo = $scope.newTodo.trim();
-		    if (!newTodo.length) {
-		        // éviter les todos vides
-		        return;
-		    }
-		    todos.push({
-		        // on ajoute le todo au tableau des todos
-		        title: newTodo,
-		        completed: false
-		    });
-		    // Réinitialisation de la variable newTodo
-		    $scope.newTodo = '';
-		};
-		
-		//Enlever un todo
-		$scope.removeTodo = function (todo) {
-		    todos.splice(todos.indexOf(todo), 1);
-		};
-		
-		// Cocher / Décocher tous les todos
-		$scope.markAll = function (completed) {
-		    todos.forEach(function (todo) {
-		        todo.completed = !completed;
-		    });
-		};
-		
-		// Enlever tous les todos cochés
-		$scope.clearCompletedTodos = function () {
-		    $scope.todos = todos = todos.filter(function (todo) {
-		        return !todo.completed;
-		    });
-		};*/
-	}
-
-]);
+	};
+	
+	$scope.startVoiture = function() {
+		$scope.vitesseA = '12';
+	};
+	
+});
 
